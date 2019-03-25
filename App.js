c3be84f682a41e268601b98129dbe4bb457448b1
Nikita S <nikita.subbotin@itomy.ch>
2019-03-25 13:54:42 +0200

    Created nested screens

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import {
+  createBottomTabNavigator,
+  createAppContainer,
+  createStackNavigator
+} from "react-navigation";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import AuthScreen from "./screens/AuthScreen";
+import MapScreen from "./screens/MapScreen";
+import DeckScreen from "./screens/DeckScreen";
+import ReviewScreen from "./screens/ReviewScreen";
+import SettingScreen from "./screens/SettingScreen";
 
 const MainNavigator = createBottomTabNavigator({
   welcome: { screen: WelcomeScreen },
-  auth: { screen: AuthScreen }
+  auth: { screen: AuthScreen },
+  main: {
+    screen: createBottomTabNavigator({
+      map: { screen: MapScreen },
+      deck: { screen: DeckScreen },
+      review: {
+        screen: createStackNavigator({
+          review: { screen: ReviewScreen },
+          settings: { screen: SettingScreen }
+        })
+      }
+    })
+  }
 });
 const Container = createAppContainer(MainNavigator);
-
 
 class App extends React.Component {
   render() {
